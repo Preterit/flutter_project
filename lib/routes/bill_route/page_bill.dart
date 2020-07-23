a12a66de2a8ddb1f433d31a9fc32e0cfb1337181
lwb <18614005205@163.com>
2020-07-23 13:44:49 +0800

    账单页面，标题栏。

--- lib/routes/bill_route/page_bill.dart
+++ lib/routes/bill_route/page_bill.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutterproject/common/toast.dart';
+import 'package:flutterproject/util/imgutil.dart';
 
 /*
  * 账单
@@ -12,10 +15,52 @@ class BillPage extends StatefulWidget {
 class _BillPageState extends State<BillPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text("账单"),
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return Container(
+      height: 150.0,
+      width: double.infinity,
+      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 15),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            Img.allUrl("ic_home_top_bg.png"),
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Center(
+                child: Text(
+                  "账单",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                margin: EdgeInsets.only(right: 10.0),
+                child: InkWell(
+                  onTap: _moreClick,
+                  child: Image.asset(
+                    Img.allUrl("ic_home_record_more.png"),
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
+}
+
+/// 更多
+void _moreClick() {
+  ToastUtil.show("more");
 }
