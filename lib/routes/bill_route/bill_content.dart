6423dd6c2a2064004492d0121709bbbc7194d5d7
lwb <18614005205@163.com>
2020-07-23 17:03:18 +0800

    切换列表， 列表标题

--- lib/routes/bill_route/bill_content.dart
+++ lib/routes/bill_route/bill_content.dart
@@ -4,8 +4,10 @@
 * Desc: 账单页面 - 内容区域
 */
 import 'package:flutter/cupertino.dart';
+import 'package:flutterproject/common/toast.dart';
 
 import 'bill_content_chart.dart';
+import 'bill_content_list_title.dart';
 
 class BillContent extends StatefulWidget {
   final dynamic item;
@@ -28,14 +30,23 @@ class _BillContentState extends State<BillContent> {
           ),
           SliverToBoxAdapter(
             ///  内容区域图标部分
-            child: BillContentChart(),
+            child: BillContentListTitle(_listTypeChange),
           ),
-          SliverToBoxAdapter(
-            ///  内容区域图标部分
-            child: BillContentChart(),
-          ),
+          SliverFixedExtentList(
+            delegate: SliverChildBuilderDelegate(
+                  (c, i) => Text("sjsjsjs"),
+              childCount: data.length,
+            ),
+            itemExtent: 48.0,
+          )
         ],
       ),
     );
+  }
+
+  void _listTypeChange(order_type){
+    setState(() {
+      ToastUtil.show("排序类型 切换 -- $order_type");
+    });
   }
 }
---
+++ lib/routes/bill_route/bill_content_list_title.dart
@@ -0,0 +1,69 @@
+/*
+* Date:2020/7/23
+* author:lwb
+* Desc:  柱状图下面 ，列表上面 账单明细
+*/
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+//enum OrderType {
+//  ORDER_BY_TIME,
+//  ORDER_BY_PRICE,
+//}
+
+class BillContentListTitle extends StatefulWidget {
+  final Function listTypeChange;
+
+  BillContentListTitle(this.listTypeChange);
+
+  @override
+  _BillContentListTitleState createState() => _BillContentListTitleState();
+}
+
+class _BillContentListTitleState extends State<BillContentListTitle> {
+  var order_type = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0, left: 15.0, right: 15.0, bottom: 20.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              "账单明细",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: listTypeChange,
+            child: Container(
+              padding: EdgeInsets.only(
+                  top: 3.0, left: 12.0, right: 12.0, bottom: 3.0),
+              child: Text(
+                order_type == 1 ? "按时间" : "按金钱",
+                style: TextStyle(fontSize: 13.0),
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.all(Radius.circular(3.0)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 切换 列表 排列顺序。
+  void listTypeChange() {
+    setState(() {
+      order_type == 1 ? order_type = 2 : order_type = 1;
+      widget.listTypeChange(order_type);
+    });
+  }
+}
